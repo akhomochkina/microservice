c860355e3ea9e33636f26db93c9dbd391bae5541
Anastasia Khomochkina <akhomochkina@gmail.com>
2022-07-21 18:36:51 -0400

    Added tests for getbyid and getbyidinfo

---
+++ tests/unit/getbyid.test.js
@@ -0,0 +1,44 @@
+// tests/unit/getbyid.test.js
+
+const request = require('supertest');
+
+const app = require('../../src/app');
+
+describe('GET /v1/fragments/:id', () => {
+  // If the request is missing the Authorization header, it should be forbidden
+  test('unauthenticated requests are denied', () => request(app).get('/v1/fragments').expect(401));
+
+  // If the wrong username/password pair are used (no such user), it should be forbidden
+  test('incorrect credentials are denied', () =>
+    request(app).get('/v1/fragments').auth('invalid@email.com', 'incorrect_password').expect(401));
+
+  // Using a valid username/password pair should give a success result with a .fragments array
+  test('authenticated users get fragment by id', async () => {
+    const res = await request(app)
+      .post('/v1/fragments')
+      .send('test fragment')
+      .set('Content-type', 'text/plain')
+      .auth('user1@email.com', 'password1');
+    var id = JSON.parse(res.text).fragment.id;
+    const res2 = await request(app).get(`/v1/fragments/${id}`).auth('user1@email.com', 'password1');
+    expect(res2.statusCode).toBe(200);
+  });
+
+  test('authenticated users get fragment by id', async () => {
+    const res = await request(app)
+      .post('/v1/fragments')
+      .send('test fragment')
+      .set('Content-type', 'text/markdown')
+      .auth('user1@email.com', 'password1');
+    var id = JSON.parse(res.text).fragment.id;
+    const res2 = await request(app)
+      .get(`/v1/fragments/${id}.html`)
+      .auth('user1@email.com', 'password1');
+    expect(res2.statusCode).toBe(200);
+  });
+
+  test('getting an error message when fragment id doe not exist', async () => {
+    const res2 = await request(app).get(`/v1/fragments/1`).auth('user1@email.com', 'password1');
+    expect(res2.statusCode).toBe(500);
+  });
+});
